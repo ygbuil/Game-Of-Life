ceec3ae680bb479d26b64593225d1af12fd6b096
Llorenç Buil <llorenc.buil@pospotential.com>
2023-02-06 17:51:25 +0100

    folder reestructure

---
+++ src/main/main.py
@@ -0,0 +1,53 @@
+# libraries
+import os
+import sys
+import time
+
+# root path
+path = 'C:\\Users\\llorenc.buil\\Documents\\github\\Game-Of-Life'
+os.chdir(path)
+if path not in sys.path:
+    sys.path.append(path)
+
+# local libraries
+from src.objects import functions as f
+
+
+def main(matrix_size, initial_probability_of_life, steps):
+    '''
+    Runs the entire simulation.
+
+    Parameters
+    ----------
+    matrix_size : int
+        Size of the matrix.
+    initial_probability_of_life : float
+        Initial probability of life for the matrix creatio, between 0 and 1.
+    steps : int
+        Number of iterations to perform.
+
+    Returns
+    -------
+    matrix : numpy array
+        Final matrix configuration.
+
+    '''
+
+    matrix = f.init_matrix(
+        matrix_size=matrix_size,
+        initial_probability_of_life=initial_probability_of_life
+    )
+
+    for _ in range(steps):
+        matrix = f.update_matrix(matrix=matrix)
+
+        f.plot_matrix(matrix)
+
+        # sleep 0.5s to see evolution
+        time.sleep(0.5)
+
+    return matrix
+
+
+# run simulation
+main(matrix_size=20, initial_probability_of_life=0.2, steps=100)
